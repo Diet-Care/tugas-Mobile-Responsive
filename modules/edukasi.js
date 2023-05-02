7a549d1282089b3268951a76d7765dbaab4e0d5a
mr-exploit <raffiaj8@gmail.com>
2023-05-02 20:22:37 +0700

    perubahan button

--- modules/edukasi.js
+++ modules/edukasi.js
@@ -1,6 +1,6 @@
 export async function edukasi(){
     try {
-          const response = await fetch("https://6436ce2c3e4d2b4a12dc3844.mockapi.io/api/v1/edukasi");
+          const response = await fetch("https://6436ce2c3e4d2b4a12dc3844.mockapi.io/api/edukasi");
           
         return parseRes(await response.json());
     } catch (error) {
@@ -24,32 +24,5 @@ function parseRes(res){
         `;
     });
 }
-
-// export async function edukasi(){
-//     try {
-//         const images = document.getElementById('image');
-//           const response = await fetch("https://6436ce2c3e4d2b4a12dc3844.mockapi.io/api/v1/edukasi").then((res) => res.json()).then((res) =>{
-//             res.forEach((edukasi) => {
-//                 images.innerHTML +=  `
-//                 <div class="col-3 mb-5" >
-//                 <div class="card">
-//                 <img src="${edukasi.image}" class="card-img-top" alt="...">
-//                     <div class="card-body">
-//                     <h5 class="card-title">${edukasi.judul}</h5>
-//                     <p class="card-text">${edukasi.deskripsi}</a>
-//                     </div>
-//                     </div>
-//                     </div>    
-//                 `;
-//             });
-//             return response.json();
-//           })
-         
-//             // console.log(await response.json());
-//             // return edu;
-//     } catch (error) {
-//           console.error("ini ada yang error",error);
-//     }
-// }
 
 
